701364b837a89cf7176a85ada8dbf867258b8af1
P L K RAGHAVENDRA <155468147+raghavendra2006@users.noreply.github.com>
2026-04-07 07:56:21 +0530

    Add mock beans for S3Client and S3Service

--- src/test/java/com/student/Student_Management/StudentManagementApplicationTests.java
+++ src/test/java/com/student/Student_Management/StudentManagementApplicationTests.java
@@ -1,13 +1,21 @@
 package com.student.Student_Management;
 
+import com.student.service.S3Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class StudentManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private S3Client s3Client;
 
+    @MockBean
+    private S3Service s3Service;
+
+    @Test
+    void contextLoads() {
+    }
 }
